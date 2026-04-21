ca6c646e6257319313b35fc677d73dc354dd4844
ATUL KUMAR <atul.0298@gmail.com>
2026-04-21 17:13:55 +0200

    Create Bimodality_Test_UKBB_Sex_Based.R

---
+++ R_Code/Bimodality_Test_UKBB_Sex_Based.R
@@ -0,0 +1,48 @@
+library (data.table)
+library(diptest)
+library(ggplot2)
+library(dplyr)
+
+TABLE <- as.data.frame(matrix(ncol=3, nrow=2923))
+names(TABLE) <- c("Protein", "Dip_Statistic", "P")
+
+options(scipen = 999) ## To avoid scientific notation
+df_Olink <- fread ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Merja_Sui_Olink_Project/7_Olink_Endocrine_Control.txt")
+
+oldnames <- c ('ERVV-1', 'HLA-A', 'HLA-DRA', 'HLA-E')
+newnames <- c ('ERVV_1', 'HLA_A', 'HLA_DRA', 'HLA_E')
+
+df_Olink <- df_Olink %>% rename_at(vars (oldnames), ~ newnames)
+
+df_2 <- df_Olink [,1:693]
+
+df_Olink_Male <- subset(df_Olink, df_Olink$Sex == 1)
+df_Olink_Female <- subset(df_Olink, df_Olink$Sex == 0)
+
+j <- 1
+
+for (i in colnames (df_Olink_Male)){
+  if (i %in% colnames (df_2)) next
+  bimod_test <- dip.test (df_Olink_Male [[i]], simulate=TRUE, B=5000)
+  TABLE[j, 1] <- i
+  TABLE[j, 2] <- bimod_test$statistic
+  TABLE[j, 3] <- bimod_test$p.value
+  
+  j <- j + 1
+}
+
+write.table (TABLE, (file = paste0 ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Merja_Sui_Olink_Project/Results/UKBB_Data_Biomodality_Test_Male.txt")), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+
+j <- 1
+
+for (i in colnames (df_Olink_Female)){
+  if (i %in% colnames (df_2)) next
+  bimod_test <- dip.test (df_Olink_Female [[i]], simulate=TRUE, B=5000)
+  TABLE[j, 1] <- i
+  TABLE[j, 2] <- bimod_test$statistic
+  TABLE[j, 3] <- bimod_test$p.value
+  
+  j <- j + 1
+}
+
+write.table (TABLE, (file = paste0 ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Merja_Sui_Olink_Project/Results/UKBB_Data_Biomodality_Test_Female.txt")), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
